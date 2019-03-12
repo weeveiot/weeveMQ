5e6ad8e3ee9ec8ba003f27b9feb69a422b315e9b
Sascha Thomsen <sascha.thomsen@weeve.network>
2019-03-12 14:03:54 +0100

    Update license header

--- WeeveClientLibrary/tool/WeeveCertificateTool.c
+++ WeeveClientLibrary/tool/WeeveCertificateTool.c
@@ -1,4 +1,15 @@
-/* INSERT LICENSE HERE */
+/* Licensed to weeveMQ under one or more contributor license agreements.
+* See the LICENCE file distributed with this work for additional information
+* regarding copyright ownership. You may obtain a copy of the License at
+*
+*     https://github.com/weeveiot/weeveMQ/blob/master/LICENCE
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 /**
  * @brief Implementation of Tool to Handle Certificate
